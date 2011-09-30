828f96bc598093b27d7459460df7b1421f55d593
fsoppelsa <fsoppelsa@rhx.it>
2011-09-30 12:22:40 +0200

    AJAX fix

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,25 +1,4 @@
 class LinksController < ApplicationController
-  # GET /links
-  # GET /links.json
-  #def index
-  #  @links = Link.all
-
-  #  respond_to do |format|
-  #    format.html # index.html.erb
-  #    format.json { render :json => @links }
-  #  end
-  #end
-
-  # GET /links/1
-  # GET /links/1.json
-  #def show
-  #  @link = Link.find(params[:id])
-
-  #  respond_to do |format|
-  #    format.html # show.html.erb
-  #  end
-  #end
-
   # GET /links/new
   # GET /links/new.json
   def new
@@ -29,11 +8,6 @@ class LinksController < ApplicationController
       format.html # new.html.erb
     end
   end
-
-  # GET /links/1/edit
-  #def edit
-  #  @link = Link.find(params[:id])
-  #end
 
   # POST /links
   # POST /links.json
@@ -59,34 +33,6 @@ class LinksController < ApplicationController
     # old links (.last is taken) are overwritten
     redirect_to Link.where("hashed = ?", @link).last.original
   end
-
-  # PUT /links/1
-  # PUT /links/1.json
-  #def update
-  #  @link = Link.find(params[:id])
-
-  #  respond_to do |format|
-  #    if @link.update_attributes(params[:link])
-  #      format.html { redirect_to @link, :notice => 'Link was successfully updated.' }
-  #      format.json { head :ok }
-  #    else
-  #      format.html { render :action => "edit" }
-  #      format.json { render :json => @link.errors, :status => :unprocessable_entity }
-  #    end
-  #  end
-  #end
-
-  # DELETE /links/1
-  # DELETE /links/1.json
-  #def destroy
-  #  @link = Link.find(params[:id])
-  #  @link.destroy
-
-  #  respond_to do |format|
-  #    format.html { redirect_to links_url }
-  #    format.json { head :ok }
-  #  end
-  #end
 
   private
 
