cdd5d3bb4c3669bfb5d77a14df70392acc478131
fsoppelsa <fsoppelsa@rhx.it>
2011-10-04 11:21:18 +0200

    recursive fix again

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -16,13 +16,11 @@ class LinksController < ApplicationController
     @link.hashed = create_hash(Rails.application.config.hash_length)
 
     respond_to do |format|
-      unless @link.original.include?(Rails.application.config.server_name[:host])
-        if @link.save
+        if @link.save 
           format.js
         else
           format.html { render :action => "new" }
         end
-      end
     end
   end
 
--- app/models/link.rb
+++ app/models/link.rb
@@ -8,6 +8,8 @@ class Link < ActiveRecord::Base
   validates_format_of :original, :with => URI::regexp(%w(http https ftp))  
   # ..it must be longer than shorter link, otherwise is useless
   validates_length_of :original, :minimum => @@minimum_length
+  # ... must not be recursive
+  validate :original_must_not_be_recursive
   
   public
 
@@ -17,5 +19,11 @@ class Link < ActiveRecord::Base
 
   def original_length
     return self.original.length
+  end
+
+  private
+
+  def original_must_not_be_recursive
+    errors.add(:original, "recursive") if original.include?(Rails.application.config.server_name[:host].to_s)
   end
 end
