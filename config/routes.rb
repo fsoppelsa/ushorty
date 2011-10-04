e2b2acfb801ebe443311f7212c28cf39b991395e
fsoppelsa <fsoppelsa@rhx.it>
2011-10-04 16:34:52 +0200

    Fixed favourite

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Ushorty::Application.routes.draw do
   get "error_page", :to => "error_page#error_page"
 
   resources :links, :exclude => [:index, :show, :destroy]
-  match '/?myurl' => 'links#ws_url'
   match '/:hashed' => 'links#redirect'
 end
